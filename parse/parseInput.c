17d798459bcbcbaa58a65ada2e7e8b5036c60251
v1t3ls0n <guyvitelson@gmail.com>
2022-01-13 06:26:18 +0200

    some code direction  on how to parse line input

---
+++ parse/parseInput.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef enum
+{
+    ignore,
+    parse,
+    error
+} State;
+
+typedef enum
+{
+    comma,
+    space,
+    newLine,
+    macro,
+    array,
+    label,
+    character,
+    number,
+
+} Expect;
+typedef enum
+{
+    label,
+    number,
+    macro,
+    array,
+    instruction,
+    dataInstuction,
+    command,
+    comma,
+    any
+} Parse;
+int main()
+{
+    char str[50] = "        XYZ:  .data 7,-57,+17";
+    int c = NULL;
+    int i = 0, j = 0;
+    char line[81];
+    char token[81];
+
+    State state = ignore;
+    Parse parse = any;
+    Expect expect = any;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+        {
+            if (state == ignore)
+            {
+                /*
+                build Words from line
+                */
+            }
+
+            switch (state == parse)
+            {
+            }
+        }
+
+        if (!isspace(c))
+        {
+
+            if (state == ignore)
+            {
+                c;
+
+                switch (c)
+                {
+
+                case '.':
+                {
+                    parse = instruction;
+                    expect = character;
+                }
+
+                case '+':
+                {
+                    parse = number;
+                    expect = number;
+                }
+                case '-':
+                {
+                    parse = number;
+                    expect = number;
+                }
+
+                default:
+                    break;
+                }
+            }
+
+            else if (state == parse)
+            {
+
+                switch (expect)
+                {
+
+                    /*
+                         label,
+                        number,
+                        macro,
+                        array,
+                        instruction,
+                        dataInstuction,
+                        command,
+                        comma,
+                        any
+                    */
+                case comma:
+                {
+                }
+
+                case number:
+                {
+                }
+
+                case character:
+                {
+                }
+
+                case instruction:
+                {
+                }
+
+                default:
+                    break;
+                }
+            }
+            state = parse;
+            token[j++] = (char)c;
+        }
+        else if (isspace(c) && state == parse)
+        {
+
+            j = 0;
+            expect = any;
+            memset(token, 0, j);
+            state = ignore;
+        }
+        else if (isspace(c) && state == ignore)
+        {
+            /*
+            do nothing
+            */
+        }
+    }
+}
