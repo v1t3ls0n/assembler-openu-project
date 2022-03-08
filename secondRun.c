45eef54aa2c15556b235eef6914894753ef08836
v1t3ls0n <guyvitelson@gmail.com>
2022-03-08 19:48:55 +0200

    save secondRun.c new file

---
+++ secondRun.c
@@ -0,0 +1,8 @@
+/*
+
+Symbol table is fully updated with all of the symbols with correct
+values (.data/.string/.entry arguments address is updated as well to start after last
+operation address)
+
+
+ */
