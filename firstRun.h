5aac488e5b5f0cf62b796929a4a7cbf41af9443c
v1t3ls0n <guyvitelson@gmail.com>
2022-03-28 21:25:13 +0300

    save

--- firstRun.h
+++ firstRun.h
@@ -1,4 +1,23 @@
 #include "firstRun.c"
+/*
+   In FirstRun.c we are parsing and analysizing the assembly code we got after we extracted the macros from it
+   we verify that the syntax is legal and that the use of the assembly language follows the rules of our
+   operations and instructions (operands type match, instruction type match, comment type match, legal declaretion
+   of symbols, etc). If we encounters error we yields (print to error.log file and to stderr) the specific error and
+   we continue checking next characters in line. For each line of code.
+
+   If no errors encountered then we will only INCREASE the DATA COUNTER or the INSTRUCTION COUNTER
+   correspondingly by the size that the line of code should take in memory (we calculate the size
+   of each instruction or operation in this proccess as well).
+
+   By doing so, at the end of the first run we will know exacly how much space we will need for the data image and how
+   much space we need for the instruction image. Doing so allows us to allocate just the required amount of memory
+   needed without wasting big random chuncks of memory upfront. If everything goes well, means that the  first Run end
+   without errors, then we set the sharedState ((*globalState)()) to secondRun.
+
+
+
+ */
 
 Bool handleOperation(char *operationName, char *args);
 
