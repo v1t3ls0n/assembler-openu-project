4cf3a582e15af13ef4f4aab6a82d3906c0c13b97
v1t3ls0n <guyvitelson@gmail.com>
2022-03-28 12:18:24 +0300

    GREATTTTTTTTTTTTTTTTT

--- linuxtest.sh
+++ linuxtest.sh
@@ -2,7 +2,7 @@ clear
 rm -rf  *.log ./_tests/good_code/code/*.log ./_tests/good_code/code/*.am ./_tests/good_code/code/*.ob ./_tests/good_code/code/*.ent ./_tests/good_code/code/*.ext ./_tests/good_code/code/*.warnings ./_tests/good_code/code/*.warnings.warnings ./_tests/good_code/code/*.errors ./_tests/good_code/code/*.errors.errors  
 rm -rf  *.log *.am *.ob *.ent *.ext *.warnings *.warnings *.errors *.errors.errors  
 make 
-./compiler test
+./compiler test-from-book
 rm -f compiler 
 # wrongNumOfOperands wrongUseOfInstructions wrongAddressingMethodTest
 # wrongAddressingMethodTest wrongNumOfOperands wrongUseOfInstructions wrongNumOfCommas
