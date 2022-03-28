9c66ba139aaca87afc15b3c62099f1b67ce482be
ShaharTorati <shahartorati@gmail.com>
2022-03-28 20:06:19 +0300

    g

--- headers/variables/complex_typedef.h
+++ headers/variables/complex_typedef.h
@@ -1,18 +1,18 @@
 #include "constants.h"
 
-/*Bit is is a special variable that we made to build with BinaryWord*/
-typedef struct
+/ Bit is is a special variable that we made to build with BinaryWord /
+    typedef struct
 {
     unsigned int on : 1;
 } Bit;
-/*BinaryWord is a struct for holding memory and data image in memory format*/
+/BinaryWord is a struct for holding memory and data image in memory format/
 typedef struct
 {
     Bit digit[BINARY_WORD_SIZE];
 } BinaryWord;
 
-/*Like BinaryWord, it represent a word with 5 hexa decimal characters in the order we will output to th .ob file*/
-typedef struct
+/ Like BinaryWord, it represent a word with 5 hexa decimal characters in the order we will output to th.ob file /
+                       typedef struct
 {
     unsigned int _A : 4;
     unsigned int _B : 4;
@@ -33,22 +33,22 @@ typedef struct
     unsigned int reg : 1;
 } AddrMethodsOptions;
 
-/*Operation is a struct for holding all the operation table data*/
+/Operation is a struct for holding all the operation table data/
 typedef struct
 {
-    unsigned int op;        /*op- is the binary value of the first sixteen bits in the first word for each operation*/
-    unsigned int funct : 4; /*the function code of the operation*/
-    char keyword[4];        /*the keyword for the operation name*/
-    AddrMethodsOptions src; /*legal addressing mehods for the source operand*/
-    AddrMethodsOptions des; /*legal addressing mehods for the destination operand*/
+    unsigned int op;        /op- is the binary value of the first sixteen bits in the first word for each operation/
+    unsigned int funct : 4;
+    / the function code of the operation /
+        char keyword[4];        /the keyword for the operation name/
+    AddrMethodsOptions src; /legal addressing mehods for the source operand/
+    AddrMethodsOptions des; /legal addressing mehods for the destination operand/
 } Operation;
 
-/*ItemType is an identyfier parameter that we use in the hash table functions that both the macro table and the symbol table are built with*/
-typedef enum
-{
-    Macro,
-    Symbol
-} ItemType;
+/ ItemType is an identyfier parameter that we use in the hash table functions that both the macro table and the symbol table are built with /
+    typedef enum {
+        Macro,
+        Symbol
+    } ItemType;
 /*DataType is an identyfier parameter that we use in the memory.c file functions in order to specify if a current memory word belongs to the data
 image part or the instruction part of the resulted image*/
 typedef enum
@@ -65,6 +65,15 @@ typedef struct
     int end;
 } MacroData;
 
+/* ExtPositionData is the nested singly linked list element in the ExtListItem that we just mentioned above.*/
+
+typedef struct ExtPositionData
+{
+    unsigned base;
+    unsigned offset;
+    struct ExtPositionData *next;
+} ExtPositionData;
+
 /* ExtListItem is a typedef struct of a single external symbol name
 that is used as operand in the assembly code. This item is the building block
 of the external operands nested singly linked list. The nested list is a singly linked list
@@ -75,16 +84,16 @@ typedef struct ExtListItem
     char name[MAX_LABEL_LEN];
     ExtPositionData value;
     struct ExtListItem *next;
-
 } ExtListItem;
 
-/* ExtPositionData is the nested singly linked list element in the ExtListItem that we just mentioned above.*/
-typedef struct ExtPositionData
+/Attributes for the type of symbol that are saved in the ymbol table/
+typedef struct
 {
-    unsigned base;
-    unsigned offset;
-    struct ExtPositionData *next;
-} ExtPositionData;
+    unsigned int code : 1;
+    unsigned int data : 1;
+    unsigned int entry : 1;
+    unsigned int external : 1;
+} Attributes;
 
 /*SymbolData is a struct that holds the additional unique data fields in the symbols hash table.
  The value member represents the  value of the memory address that the symbol appearse in.
@@ -101,16 +110,8 @@ typedef struct
     Attributes attrs;
 } SymbolData;
 
-/*Attributes for the type of symbol that are saved in the ymbol table*/
-typedef struct
-{
-    unsigned int code : 1;
-    unsigned int data : 1;
-    unsigned int entry : 1;
-    unsigned int external : 1;
-} Attributes;
-/*Item is the building block of the hash table data structure that both the symbols and macros tables are built with*/
-typedef struct Item
+/ Item is the building block of the hash table data structure that both the symbols and macros tables are built with /
+    typedef struct Item
 {
     char name[MAX_LABEL_LEN];
     union
