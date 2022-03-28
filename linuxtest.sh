af31c4619d265e1336189f32ec95c0c2e58e2bbb
v1t3ls0n <guyvitelson@gmail.com>
2022-03-28 12:14:33 +0300

    SAVEEEEEEEEEE

--- linuxtest.sh
+++ linuxtest.sh
@@ -2,7 +2,8 @@ clear
 rm -rf  *.log ./_tests/good_code/code/*.log ./_tests/good_code/code/*.am ./_tests/good_code/code/*.ob ./_tests/good_code/code/*.ent ./_tests/good_code/code/*.ext ./_tests/good_code/code/*.warnings ./_tests/good_code/code/*.warnings.warnings ./_tests/good_code/code/*.errors ./_tests/good_code/code/*.errors.errors  
 rm -rf  *.log *.am *.ob *.ent *.ext *.warnings *.warnings *.errors *.errors.errors  
 make 
-./compiler test wrongAddressingMethodTest wrongNumOfOperands wrongUseOfInstructions wrongAddressingMethodTest
+./compiler test
 rm -f compiler 
 # wrongNumOfOperands wrongUseOfInstructions wrongAddressingMethodTest
 # wrongAddressingMethodTest wrongNumOfOperands wrongUseOfInstructions wrongNumOfCommas
+# test wrongAddressingMethodTest wrongNumOfOperands wrongUseOfInstructions wrongAddressingMethodTest
